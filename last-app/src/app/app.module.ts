0e80be8d2b8be484b2e97160480355603a59b940
Dihia <siliana.aitsmail@gmail.com>
2021-05-21 21:53:17 +0100

    add new components

--- last-app/src/app/app.module.ts
+++ last-app/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { HeaderPageComponent } from './components/partials/header-page/header-page.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    HeaderPageComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent,
+    ResetPasswordComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
